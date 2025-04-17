0ac95ad99469a8d54b1a3dc5901394571f571e9a
prasanna <prasanna.gramopadhye@optanium.com>
2025-04-17 08:29:14 +0530

    Initial Commit

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+// jest.config.ts
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
+
+export default config;
